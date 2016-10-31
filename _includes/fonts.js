2aab4cd8f57308f26b929cde8d407cd64f39fe4f
Andrew White <andrew.white@unboxed.co>
2016-10-31 13:01:28 +0000

    Strip back to basic HTML ready for responsive design

---
+++ _includes/fonts.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+  var MTIProjectId='b73921bb-d4bf-4a77-bd41-79c3da8c9fec';
+
+  (function() {
+    var headElement = document.getElementsByTagName('head')[0];
+    var bodyElement = document.getElementsByTagName('body')[0];
+    var element = headElement || bodyElement;
+    var mtiTracking = document.createElement('script');
+
+    mtiTracking.type='text/javascript';
+    mtiTracking.async='true';
+    mtiTracking.src='{{ site.baseurl }}/mtiFontTrackingCode.js';
+    element.appendChild(mtiTracking);
+  })();
+</script>
